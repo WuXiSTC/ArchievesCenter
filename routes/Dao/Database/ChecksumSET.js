5b3b7f70920bd9f5db79854f173838d4471073a4
yindaheng98 <yindaheng98@163.com>
2019-12-11 15:34:29 +0800

    校验码写入数据库已完成

--- routes/Dao/Database/ChecksumSET.js
+++ routes/Dao/Database/ChecksumSET.js
@@ -1,6 +1,17 @@
-function ChecksumSET(checksums, filename) {
-    //TODO:通向数据库中插入校验码和文件路径
-    //checksums为要插入的校验码列表，其中的校验码数量可能有多个（一个文件由不同算法算出的多个校验码）
-    //checksums的校验码的排列顺序必须固定
-    //filename为要插入的文件路径
+const connection = require("./connection");
+const ChecksumKey = require("./ChecksumKey");
+
+/**
+ * 通向数据库中插入校验码和文件路径
+ * @param checksums 为要插入的校验码列表，其中的校验码数量可能有多个（一个文件由不同算法算出的多个校验码）
+ * @param filename 为要插入的文件路径
+ * @returns {Promise<void>}
+ * @constructor
+ */
+async function ChecksumSET(checksums, filename) {
+    await connection.hset("file", filename, JSON.stringify(checksums));
+    for (let alg of Object.keys(checksums))
+        await connection.sadd(ChecksumKey(alg, checksums[alg]), filename);
 }
+
+module.exports = ChecksumSET;
--- routes/Dao/Database/connection.js
+++ routes/Dao/Database/connection.js
@@ -1,1 +1,30 @@
-const redis= require("redis");
+const redis = require("redis");
+
+let client = redis.createClient("redis://127.0.0.1:9221", {
+    retry_strategy: function (options) {
+        if (options.error && options.error.code === 'ECONNREFUSED')
+            console.error('连接被拒绝');
+        console.warn('重试连接: ' + options.times_connected);
+        return Math.max(options.attempt * 100, 3000);
+    }
+});
+client.on("error", (e) => {
+    console.error("Redis 连接出错: " + e);
+});
+
+const supported_commands = ['hset', 'sadd', 'smembers'];
+
+function Connection() {
+    for (let command of supported_commands)
+        this[command] = function () {
+            let args = arguments;
+            return new Promise((resolve, reject) => {
+                client[command](...args, (error, result) => {
+                    if (error) return reject(error);
+                    return resolve(result)
+                })
+            })
+        }
+}
+
+module.exports = new Connection();
